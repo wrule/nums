4644a649573d1311815b91a54210596e8fc6a4b5
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-27 15:06:43 +0800

    测试速度

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,16 @@
 
 import { nums } from './index';
 
-const a = nums([1, 2, 3]);
-console.log(a.Nums);
-console.log(a.avg());
-console.log(a.MA(2));
-console.log(a.EMA(2));
+const data = Array(365 * 8).fill(0).map(() => Math.random() * 100000);
+const dataNums = nums(data);
+
+const oldTime = Number(new Date());
+for (let i = 0; i < 120; ++i) {
+  for (let j = 0; j < 120; ++j) {
+    const a = dataNums.EMA(i);
+    const b = dataNums.EMA(j);
+  }
+}
+console.log(Number(new Date()) - oldTime);
+
+console.log('结束');
