2f83ef99468244e4dd8ec8bc96873178282fcccf
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-27 17:22:51 +0800

    RSV测试

--- src/test.ts
+++ src/test.ts
@@ -4,13 +4,13 @@ import { nums } from './index';
 const data = Array(365 * 8).fill(0).map(() => Math.random() * 100000);
 const dataNums = nums(data);
 
-// const oldTime = Number(new Date());
-// for (let i = 0; i < 120; ++i) {
-//   for (let j = 0; j < 120; ++j) {
-//     const a = dataNums.RSV(10);
-//   }
-// }
-// console.log(Number(new Date()) - oldTime);
+const oldTime = Number(new Date());
+for (let i = 0; i < 120; ++i) {
+  for (let j = 0; j < 120; ++j) {
+    const a = dataNums.RSV(10);
+  }
+}
+console.log(Number(new Date()) - oldTime);
 
 console.log('结束');
 
