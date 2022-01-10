6aec5d58d7e74b1bfab040709261683ae3a3495b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-10 14:18:05 +0800

    使用新的数据源

--- src/test.ts
+++ src/test.ts
@@ -1,19 +1,4 @@
-
 import { nums } from './index';
-
-const ENS = nums([
-  73.82, 55.31, 54.65, 55.57, 56.21,
-  55.77, 48.17, 48.16, 41.31, 47.84,
-  46.78, 42.06, 40.97, 50.80, 49.17,
-  68.31, 59.51, 73.45, 76.15, 70.21,
-  69.93, 64.81, 61.67, 54.24, 50.81,
-  49.30, 51.42, 50.69, 49.36, 44.97,
-  42.61, 47.48, 47.06, 39.96, 39.85,
-  42.48, 44.34, 42.18, 42.62, 40.86,
-  40.39, 40.95, 45.14, 49.51, 45.65,
-  46.43, 45.66, 46.78,
-]);
-
 import Papa from 'papaparse';
 
 const csvData = Papa.parse(`
@@ -85,9 +70,6 @@ time,open,high,low,close,%K,%D
   dynamicTyping: true,
 });
 
-const a = ENS.StochRSI(14, 25, 7, 3);
-console.log(a.D);
-// console.log(csvData.data);
 const b = nums(csvData.data.map((item: any) => item.close));
 const c = b.StochRSI(14, 25, 7, 3);
 console.log(c.D);
